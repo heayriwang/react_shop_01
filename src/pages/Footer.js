85829df159e8a745be1ead17de7f9aa6b3e4b31e
[heayriwang] <ee7615@naver.com>
2023-03-02 17:13:16 +0900

    2222

---
+++ src/pages/Footer.js
@@ -0,0 +1,12 @@
+import GlobalNav from "./GlobalNav";
+
+
+const Footer = ({ DEFAULT, NAV }) => {
+    return (
+        <footer>
+            <GlobalNav NAV={NAV} />
+        </footer>
+    )
+
+}
+export default Footer;
---
+++ src/pages/TopBanner.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { Inner, BTN } from "./common";
+import { BsX } from "react-icons/bs";
+import { useRef, useState } from "react";
+
+
+const TopBannerWapper = styled.div`
+position: relative;
+display: flex;
+align-items: center;
+height: 100px;
+background: #f9f9f9;
+overflow: hidden;
+transition: height 0.5s;
+
+&.on{
+height: 0;
+}
+
+h2{
+font-size: 20px;
+font-weight: 500;
+margin: 0 0 5px 0;
+}
+
+button{
+margin: 0 0 0 20px;
+}
+
+.X{
+position: absolute;
+inset: 50% auto auto 50%;
+margin:  0 0 0 600px;
+transform: translate(-100%,-50%);
+font-size: 30px;
+
+}
+
+`
+
+
+
+const TopBanner = ({ DEFAULT_CONTENTS }) => {
+    const [on, setOn] = useState(false);
+
+    return (
+        <TopBannerWapper className={on ? 'on' : ''}>
+            <Inner>
+                <h2> {DEFAULT_CONTENTS[0].title}</h2>
+                <p> {DEFAULT_CONTENTS[0].description} <BTN>more</BTN> </p>
+            </Inner>
+            <BsX className="X" onClick={() => setOn(true)} />
+        </TopBannerWapper >
+    )
+}
+
+export default TopBanner;
